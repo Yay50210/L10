9aaf6f1ca2a18c14d87955e70a59a33d31d05b68
Best <Photchara_k@cmu.ac.th>
2023-12-19 17:07:32 +0700

    จบไปอีกวัน

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+    ifstream source;
+    source.open("score.txt");
+    int i = 0;
+    string num;
+    string devi;
+    float sum = 0;
+    float sta = 0;
+    float m = 0;
+    while (getline(source, num))
+    {
+        sum += atof(num.c_str());
+        sta += pow(atof(num.c_str()), 2);
+        i++;
+    }
+
+    cout << "Number of data = " << i << endl;
+    cout << setprecision(3);
+    m = sum / i;
+    cout << "Mean = " << m << endl;
+    
+
+    cout << "Standard deviation = " << sqrt((sta/i) - pow(m, 2));
+    return 0;
+}
